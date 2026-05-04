19426fca22434987503c5e57383d9f191554531d
Arif Ali <2arif2143055@gmail.com>
2026-05-04 12:09:56 +0500

    Add files via upload

---
+++ FuelManagementSystem/src/model/Vehicle.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Vehicle {
+    public int vehicleId;
+    public String regNo;
+    public String model;
+
+    public Vehicle(int id, String r, String m) {
+        vehicleId = id;
+        regNo = r;
+        model = m;
+    }
+}
